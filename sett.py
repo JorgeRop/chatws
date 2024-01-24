42da0935a1e278b0f70ff5e4d7d17aaccb76e2bb
JorgeRop <jorgesr.p.jrp@gmail.com>
2024-01-24 11:09:53 -0600

    prueba1

---
+++ sett.py
@@ -0,0 +1,7 @@
+token = 'MRToken'
+
+whatsapp_token = 'EAAxq5J8OSLUBOzrBmiJ1zLAshvrXsQo3zQRrjZBIzehk3kaRrpdUBHnY6PkuDSzketPGEapZBD4SZC59pubPxAA3qCT9OCpWLAekrZCMYrK74b198Y6E5dTYIZCLS3p8evWFJlZBWaZBsrNDDfSBCS2MyOKIZBRLIo2ItcFj6vMTq9nZAaPBXehzIZAtIeSjDk4i1ZA5vhlyMuqgqZBLnd535dnKeZAwWducXUash'
+
+whatsapp_url = 'https://graph.facebook.com/v18.0/104834166023383/messages'
+
+document_url = "https://seguimiento.agoraparticipa.org/docs/PDF_TEXT-CA4Bn.pdf"
