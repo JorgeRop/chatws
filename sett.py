f051e3087048f6a33a4216782faaff9fd46bb5ad
Jorge Rodriguez <gio@iMac-de-Jorge.local>
2024-11-12 11:05:05 -0600

    key adds

--- sett.py
+++ sett.py
@@ -1,5 +1,3 @@
-token = 'MRToken'
-
-whatsapp_url = 'https://graph.facebook.com/v18.0/104834166023383/messages'
+token = 'MRTok'
 
 document_url = "https://seguimiento.agoraparticipa.org/docs/PDF_TEXT-CA4Bn.pdf"
