0f9cb91ad5332f53722f031a37f547a492be86c7
Jorge Rodriguez <gio@iMac-de-Jorge.local>
2024-11-12 10:58:38 -0600

    key

--- sett.py
+++ sett.py
@@ -1,7 +1,5 @@
 token = 'MRToken'
 
-whatsapp_token = 'EAAxq5J8OSLUBOxz49rgYDJHT8CCpLVqZCvFxY8yrKyQxTfGy380RN3g2tSpc8rgb5CJ9H0ZBuWUZCNPNyMV4Mnb3XBN8w9t2LGcZAayX0O2iu7PwiXWuLoZA7xxJYj8ZCIyMXrUHgqXBWjMBkEnDaZAtk71X8VgLOhZAyDMV9iwJ9eiE7dJADXtbnxWyjX7jhX2qTOjQsMZC5NBhnNRI1'
-
 whatsapp_url = 'https://graph.facebook.com/v18.0/104834166023383/messages'
 
 document_url = "https://seguimiento.agoraparticipa.org/docs/PDF_TEXT-CA4Bn.pdf"
